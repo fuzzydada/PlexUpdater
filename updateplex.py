48fa52f7084d833e6cf63f740bd35029eecd1200
fuzzydada <ejaujou@gmail.com>
2019-11-15 20:02:38 -0800

    Add files via upload

--- updateplex.py
+++ updateplex.py
@@ -13,17 +13,24 @@ import sys
 
 from xml.etree import ElementTree as etree
 
-PLEX_TOKEN = 'YOURTOKENHERE'
+PLEX_TOKEN = 'XXXXXXXXXXXXXXXXXX'
 PLEX_URL = 'http://localhost:32400/updater/status'
-LOG_FILE = './updateplex.log'
-DOWNLOAD_DIR = '/tmp/'
+LOG_FILE = 'updateplex.log'
+DOWNLOAD_DIR = '/tmp'
 
-logging.basicConfig(
-#    filename=ARGS.log_file,
-    format='%(asctime)s %(levelname)s\t%(funcName)s\t%(message)s',
-    datefmt='%b %d %T',
-    level=logging.DEBUG,
-)
+def set_up():
+    levels = {
+            'INFO': logging.INFO,
+            'WARN': logging.WARN,
+            'ERROR': logging.ERROR,
+            'DEBUG': logging.DEBUG,
+            }
+    logging.basicConfig(
+        filename=ARGS.log_file,
+        format='%(asctime)s %(levelname)s\t%(funcName)s\t%(message)s',
+        datefmt='%b %d %T',
+        level=levels[ARGS.log_level],
+    )
 
 def check_for_updates(url):
     """Requests plexmediaserver update status and initiates update if possible.
@@ -45,40 +52,34 @@ def check_for_updates(url):
         # write to log
         sys.exit(0)
     elif update_available == '0':
-        # write to log
         logging.debug("No update available. Bye!")
         sys.exit(0)
     else:
-        # write to log || send notification
         logging.error("Something broke while checking")
         sys.exit(1)
 
 
 def download_plex(url):
-    try:
-        update_package = requests.get(url, stream=True)
-        if ARGS.filename is None:
-            ARGS.filename = update_package.url.split('/')[-1]
-        with open(ARGS.directory + ARGS.filename, 'wb') as output_file:
-            logging.debug("Downloading...")
-            for chunk in update_package.iter_content(chunk_size=1024):
-                output_file.write(chunk)
-            update_package.close()
-            logging.debug("Update downloaded.")
-            return True
-    except Exception as e:
-        # write to log
-        print(e)
-        return False
+    update_package = requests.get(url, stream=True)
+    if ARGS.filename is None:
+        ARGS.filename = update_package.url.split('/')[-1]
+    with open(os.path.join(ARGS.directory, ARGS.filename), 'wb') as output_file:
+        logging.info("Downloading %s..." % update_package.url)
+        for chunk in update_package.iter_content(chunk_size=1024):
+            output_file.write(chunk)
+            logging.debug("Writing chunk...")
+        update_package.close()
+        logging.info("Update downloaded.")
+        return
 
 
 def update_plex(download_url):
-    if download_plex(url=download_url) == True:
+    try:
+        download_plex(url=download_url)
         subprocess.run(['/usr/bin/sudo', '/usr/bin/dpkg', '-i',
                         ARGS.directory + ARGS.filename])
-        print('That actually worked.')
-        # write to log
-    else:
+        logging.info("Success!")
+    except:
         logging.error("Something broke while downloading.")
         print("Failure.")
         sys.exit(1)
@@ -89,6 +90,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Update a local Plex Meda Server.')
     parser.add_argument('-l', '--log_file', action='store', type=str, default='./updateplex.log',
                         help='Specify a logfile destination (default: ./updateplex.log)')
+    parser.add_argument('-L', '--log_level', action='store', type=str, default='INFO',
+                        help='Logging level. One of: INFO, WARN, ERROR, DEBUG')
     parser.add_argument('-d', '--directory', action='store', type=str, default='/tmp/',
                         help='Download folder for update file (default: /tmp/)')
     parser.add_argument('-r', '--dry-run', action='store_true',
@@ -96,4 +99,5 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--filename', action='store', type=str, default=None,
                         help='Optional: specify an output filename other than as-downloaded')
     ARGS = parser.parse_args()
+    set_up()
     check_for_updates(PLEX_URL)
